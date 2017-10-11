4201c60b53b1fc57bedc30615170025b49c237a9
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-10 23:30:25 -0400

    added require pp to make the output nicer as suggested

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 digits = ['1','2','3','4','5','6','7','8','9']
 en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
@@ -7,5 +9,5 @@ translate = {}
 digits.each do |index|
   translate[index.to_i] = {english: en[index.to_i - 1], french: fr[index.to_i - 1]}
 end
-# need to start at index - for the value portion, which is why subtract one as the index variable starts @1 
-p translate
+# need to start at index - for the value portion, which is why subtract one as the index variable starts @1
+pp translate
